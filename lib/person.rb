7d0524128631d3c0383a23d19b1237ea7775d804
James Berry <84353254+JamesRynnBerry@users.noreply.github.com>
2021-07-22 11:48:32 -0400

    commit tree eed8fd139e76584c1d720f9b1fe653467101bc51

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,34 @@
 # your code goes here
+class Person
+    attr_reader :name, :bank_account, :happiness, :hygiene
+
+    def initialize(name, happiness, hygiene)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    # def person
+    #     person = Person.new(
+    #     name = "Stella",
+    #     happiness = 8,
+    #     hygiene = 8,
+    #     bank_account,
+    #     )
+    #     bank_account_change(10)
+    # person
+    # end
+
+    def bank_account_change(new_amount)
+        @bank_account += new_amount 
+    end
+
+end
+
+person = Person.new("Stella", 8 , 8)
+puts person.name
+puts person.happiness
+puts person.hygiene
+puts person.bank_account
+puts person.bank_account_change (10)
